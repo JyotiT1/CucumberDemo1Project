7f2018c64e706a21706fcf9aeef6d183dbdaf161
JyotiT1 <jyoti.innovationahead@gmail.com>
2024-09-16 01:35:21 +0530

    Update DriverFactory.java

--- src/main/java/com/demo/qa/factory/DriverFactory.java
+++ src/main/java/com/demo/qa/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package com.demo.qa.factory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -24,8 +25,8 @@ public class DriverFactory {
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");  // Run Chrome in headless mode
-	        	options.addArguments("--start-maximized");  // Start maximized (optional in headless mode)
-	       		options.addArguments("--disable-extensions");  // Disable extensions
+	        options.addArguments("--start-maximized");  // Start maximized (optional in headless mode)
+	        options.addArguments("--disable-extensions");  // Disable extensions
 			tlDriver.set(new ChromeDriver(options));
 		}
 		else if (browser.equalsIgnoreCase("firefox")) {
@@ -34,8 +35,8 @@ public class DriverFactory {
 		} else
 			System.out.println("Please pass the correct browser " + browser);
 
-		getDriver().manage().deleteAllCookies();
-		getDriver().manage().window().maximize();
+//		getDriver().manage().deleteAllCookies();
+//		getDriver().manage().window().maximize();
 
 		return getDriver();
 	}
